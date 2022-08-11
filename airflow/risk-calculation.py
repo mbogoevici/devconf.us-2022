85ba1d3a51271d4ca91599d02f2f988a36cfb620
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2022-08-11 17:14:39 -0400

    Updated DAG

--- airflow/risk-calculation.py
+++ airflow/risk-calculation.py
@@ -32,4 +32,4 @@ with DAG(dag_id="risk_calculation", start_date=pendulum.datetime(2022, 3, 4)) as
         print(f"Total was {total}")
 
 
-    pre_calculation >> post_calculation(sum_it(add_one.expand(values = generate_numbers())))
+    pre_calculation >> post_calculation(sum_it(add_one.expand(values=[1,2,3])))
