05db7899a4388f8a7dc69ca1a754ec25fce06757
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2022-08-11 11:39:43 -0400

    Added skeleton

---
+++ airflow/risk-calculation.py
@@ -0,0 +1,38 @@
+import logging
+import os
+
+import pendulum
+
+from airflow import DAG
+from airflow.configuration import conf
+from airflow.decorators import task
+from airflow.example_dags.libs.helper import print_stuff
+
+with DAG(dag_id="risk_calculation", start_date=datetime(2022, 3, 4)) as dag:
+
+
+    def generate_numbers():
+        return range(1,100)
+    
+    @task
+    def pre_calculation():
+        printf("Beginning risk calculation")
+
+    @task
+    def add_one(x: int):
+        return x + 1
+
+    @task
+    def sum_it(values):
+        total = sum(values)
+        return total
+
+    @task
+    def post_calculation(total):
+        print(f"Total was {total}")
+
+
+    added_values = add_one.expand(list(generate_numbers()))
+
+    pre_calculation >> added_values >> post_calculation
+
