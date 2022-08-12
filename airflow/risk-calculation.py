d4d56eba41349ca196b24d1ee6c045dbab9cb3e5
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2022-08-11 23:12:15 -0400

    Updated DAG

--- airflow/risk-calculation.py
+++ airflow/risk-calculation.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -7,6 +8,10 @@ from airflow import DAG
 from airflow.configuration import conf
 from airflow.decorators import task
 from airflow.example_dags.libs.helper import print_stuff
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3_copy_object import S3CopyObjectOperator
+from airflow.providers.amazon.aws.operators.s3_delete_objects import S3DeleteObjectsOperator
+
 
 with DAG(dag_id="risk_calculation", start_date=pendulum.datetime(2022, 2, 12), catchup = False) as dag:
 
@@ -15,11 +20,14 @@ with DAG(dag_id="risk_calculation", start_date=pendulum.datetime(2022, 2, 12), c
     
     @task
     def pre_calculation():
-        return generate_numbers()
+        s3_hook = S3Hook(aws_conn_id='s3')
+        file = s3_hook.read_key('portfolios.json', 'risk-calc')
+        data = json.load(file)
+        return data
 
     @task
-    def add_one(x:int):
-        return x + 1
+    def add_one(portfolio_data):
+        return portfolio_data['quantity'];
 
     @task
     def sum_it(values):
@@ -31,5 +39,5 @@ with DAG(dag_id="risk_calculation", start_date=pendulum.datetime(2022, 2, 12), c
         print(f"Total was {total}")
 
     data = pre_calculation() 
-    total = sum_it(add_one.expand(x = data))
+    total = sum_it(add_one.expand(portfolio_data = data))
     post_calculation(total)
