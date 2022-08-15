fb83cf64ccf86c33b2955fa8343eac74c04b7265
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2022-08-14 23:09:42 -0400

    Adding KubePod Task

--- airflow/risk-calculation-with-pod.py
+++ airflow/risk-calculation-with-pod.py
@@ -31,7 +31,7 @@ with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 
         # unique id of the task within the DAG
         task_id='calculate_var',
         # the Docker image to launch
-        image='fsi/var:v5',
+        image='fsi/var:v6',
         # launch the Pod on the same cluster as Airflow is running on
         in_cluster=True,
         # launch the Pod in the same namespace as Airflow is running in
