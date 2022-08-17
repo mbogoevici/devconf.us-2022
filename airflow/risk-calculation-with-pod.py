228519c307192eb92f5d7db7fb69237bb9762ca7
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2022-08-17 10:24:14 -0400

    Add value calc service

--- airflow/risk-calculation-with-pod.py
+++ airflow/risk-calculation-with-pod.py
@@ -15,19 +15,20 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.operators.python import PythonOperator
 from airflow.providers.cncf.kubernetes.utils.xcom_sidecar import PodDefaults
 from airflow.providers.http.hooks.http import HttpHook;
-from numpy import partition
 
-with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 2, 12), catchup = False, concurrency=10) as dag:
-
+with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 2, 12), catchup=False,
+         concurrency=10) as dag:
     def remove_suffix(input_string, suffix):
         if suffix and input_string.endswith(suffix):
             return input_string[:-len(suffix)]
         return input_string
 
+
     def remove_prefix(input_string, prefix):
         if prefix and input_string.startswith(prefix):
             return input_string[len(prefix):]
         return input_string
+
 
     @task
     def populate_cache():
@@ -41,8 +42,9 @@ with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 
             print(key)
             ticker = remove_prefix(remove_suffix(key, ".json"), 'market-data/')
             print(ticker)
-            HttpHook(method='PUT', http_conn_id='hazelcast').run(endpoint='rest/v2/caches/market-data/{}'.format(ticker),
-                                                   data=s3_hook.read_key(key, bucket_name='risk-calc'))
+            HttpHook(method='PUT', http_conn_id='hazelcast').run(
+                endpoint='rest/v2/caches/market-data/{}'.format(ticker),
+                data=s3_hook.read_key(key, bucket_name='risk-calc'))
 
 
     @task
@@ -51,6 +53,7 @@ with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 
         file = s3_hook.read_key('portfolios.json', 'risk-calc')
         data = json.loads(file)
         return list(map(lambda p: {'PORTFOLIO_DATA': "{}".format(json.dumps(p))}, data))
+
 
     PodDefaults.SIDECAR_CONTAINER.image = "image-registry.openshift-image-registry.svc:5000/airflow/alpine:latest"
     calculate_var = KubernetesPodOperator.partial(
@@ -80,17 +83,19 @@ with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 
         log_events_on_failure=True
     ).expand(env_vars=extract_portfolios());
 
+
     @task
     def aggregate(values):
         return values
 
+
     def _write_results_to_S3(**kwargs):
         ti = kwargs['ti']
         results = ti.xcom_pull(key='return_value', task_ids=['calculate_var'])
-        results_flat = sum(results, [])
         s3_hook = S3Hook(aws_conn_id='s3')
-        s3_hook.load_string(json.dumps(results, indent=2), bucket_name= 'risk-calc',
-                            key="results/value-at-risk-{}-{}.json".format(str(datetime.utcnow()).split()[0],str(datetime.utcnow()).split()[1]))
+        s3_hook.load_string(json.dumps(results, indent=2), bucket_name='risk-calc',
+                            key="results/value-at-risk-{}-{}.json".format(str(datetime.utcnow()).split()[0],
+                                                                          str(datetime.utcnow()).split()[1]))
 
 
     publish_results = PythonOperator(
@@ -98,6 +103,5 @@ with DAG(dag_id="risk_calculation-with-pod", start_date=pendulum.datetime(2022, 
         python_callable=_write_results_to_S3,
         provide_context=True,
         dag=dag)
-
 
     populate_cache() >> calculate_var >> publish_results
